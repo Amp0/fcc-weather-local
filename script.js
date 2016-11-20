6b140c6aa50ede39f26c00dbd4b86c7d767bfb74
Ampo0 <amedeefleau@gmail.com>
2016-11-20 17:57:36 +0100

    Added celcius

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ $(document).ready(function () {
     /* $("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
   }); */
 
-    $.getJSON("https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&appid=5975571933f7250f571be61da5667d3f", function(data) {
+    $.getJSON("https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&appid=5975571933f7250f571be61da5667d3f&units=metric", function(data) {
       $(".city").text(data.name);
       $(".logoWeather").html("<i class='owf owf-"+data.weather[0].id+"'>");
       $(".weatherDescription").text(data.weather[0].description.charAt(0).toUpperCase() + data.weather[0].description.slice(1));
+      $(".weatherTemp").text(data.main.temp+"°C");
     });
   });
 });

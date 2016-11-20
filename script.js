02e0c5bd24ce96d69506e951372fb811a7a58bd4
Ampo0 <amedeefleau@gmail.com>
2016-11-20 18:14:00 +0100

    Finished

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
   // API KEY : &APPID=5975571933f7250f571be61da5667d3f
+  var temp_F = 0;
+  var temp_C = 0;
   navigator.geolocation.getCurrentPosition(function(position) {
     /* $("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
   }); */
@@ -9,6 +11,21 @@ $(document).ready(function () {
       $(".logoWeather").html("<i class='owf owf-"+data.weather[0].id+"'>");
       $(".weatherDescription").text(data.weather[0].description.charAt(0).toUpperCase() + data.weather[0].description.slice(1));
       $(".weatherTemp").text(data.main.temp+"°C");
+      temp_C = data.main.temp;
+      temp_F = data.main.temp*9/5+32;
+      $(".btn-change").css("display", "block");
     });
+
+
+
+  });
+  $('.btn-change').click(function () {
+    if($(".btn-change").text() == "Swap to Fahrenheit"){
+      $(".weatherTemp").text(temp_F+"°F");
+      $(".btn-change").text("Swap to Celcius");
+    } else {
+      $(".weatherTemp").text(temp_C+"°C");
+      $(".btn-change").text("Swap to Fahrenheit");
+    }
   });
 });
